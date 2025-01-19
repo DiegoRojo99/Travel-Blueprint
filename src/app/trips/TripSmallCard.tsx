839946a452833e029022902b9b167e69ad597e3b
DiegoRojo99 <diego99rojo@gmail.com>
2025-01-19 19:41:40 +0000

    Small trip card style update

--- src/app/trips/TripSmallCard.tsx
+++ src/app/trips/TripSmallCard.tsx
@@ -1,15 +1,28 @@
 import { Trip } from "@/types/trip";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendar, faMapPin } from '@fortawesome/free-solid-svg-icons';
+
+function formatDate(date: string) {
+  return new Date(date).toLocaleDateString('en-US', {
+    month: 'short',
+    day: 'numeric',
+  });
+}
 
 const TripSmallCard = ({ trip }: { trip: Trip }) => (
   <div
     key={trip.id}
-    className="bg-gray-100 p-4 rounded shadow-md flex flex-col gap-2 text-black"
+    className="bg-gray-100 p-4 rounded shadow-md flex flex-col text-black"
   >
     <span className="font-semibold">{trip.name}</span>
-    <span>
-      {trip.startDate} - {trip.endDate}
+    <span className="mt-2">
+      <FontAwesomeIcon icon={faCalendar} size="sm" className="text-gray-500 mr-1" />
+      {formatDate(trip.startDate)} - {formatDate(trip.endDate)}
     </span>
-    <span>{trip.destination}</span>
+    <span>
+      <FontAwesomeIcon icon={faMapPin} size="1x" className="text-gray-500 mr-1" />
+      {trip.destination}
+    </span>
   </div>
 );
 
