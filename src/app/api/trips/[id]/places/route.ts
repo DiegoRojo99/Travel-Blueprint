0f688cb705b4528abfac1849c100cca00b059543
DiegoRojo99 <diego99rojo@gmail.com>
2025-01-21 21:58:27 +0000

    Places POST endpoint created

---
+++ src/app/api/trips/[id]/places/route.ts
@@ -0,0 +1,59 @@
+import { getTripById } from "@/db/trips";
+import crypto from 'crypto';
+import { updateTripPlaces } from "../../../../../db/places";
+import { Trip } from "@/types/trip";
+
+interface params {
+  id: string;
+  [key: string]: string;
+}
+
+interface context {
+  params: Promise<params>;
+}
+
+export async function POST(req: Request, context: context): Promise<Response> {
+  const { params } = context;
+  const { id } = await params;
+
+  let placeData;
+  try {
+    const body = await req.text();
+    if (!body) {
+      return new Response('Bad Request: Empty body', { status: 400 });
+    }
+    placeData = JSON.parse(body);
+    if (!placeData) {
+      throw new Error('Invalid JSON');
+    }
+  } 
+  catch (error) {
+    console.error('Error parsing JSON', error);
+    return new Response('Bad Request: Invalid JSON', { status: 400 });
+  }
+
+  try {
+    const trip: Trip | null = await getTripById(id);
+    if (!trip) {
+      return new Response('Not Found: Trip not found', { status: 404 });
+    }
+
+    const newPlace = {
+      id: crypto.randomUUID(),
+      ...placeData,
+    };
+
+    let places = trip.places || [];
+    places.push(newPlace);
+
+    let updateResult = await updateTripPlaces(id, places);
+    if (!updateResult) {
+      return new Response('Error adding stop', { status: 500 });
+    }
+
+    return new Response(JSON.stringify(newPlace), { status: 201 });
+  } catch (error) {
+    console.error('Error adding stop', error);
+    return new Response('Error adding stop', { status: 500 });
+  }
+}
