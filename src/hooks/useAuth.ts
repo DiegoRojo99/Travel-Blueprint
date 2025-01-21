abd2abca289ad263230d3d09902217171ad6e4c5
DiegoRojo99 <diego99rojo@gmail.com>
2025-01-21 00:22:56 +0000

    Error messages updated

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut } from 'firebase/auth';
 import { auth } from '@/utils/firebase';
+import { FirebaseError } from 'firebase/app';
 
 interface User {
   uid: string;
@@ -29,14 +30,30 @@ export const useAuth = () => {
     try {
       await signInWithEmailAndPassword(auth, email, password);
       return true;
-    } catch (error: unknown) {
-      if (error instanceof Error) {
+    } 
+    catch (error: unknown) {
+      if (error instanceof FirebaseError) {
+        console.error("Firebase Error during login:", error);
+        if (error.code === 'auth/user-not-found') {
+          setError('User not found');
+        }
+        else if (error.code === 'auth/invalid-credential') {
+          setError('Invalid credentials');
+        }
+        else {
+          setError('An unknown error occurred');
+        }
+      }
+      else if (error instanceof Error) {
+        console.error("Error during login:", error);
         setError(error.message);
-      } else {
+      }
+      else {
         setError('An unknown error occurred');
       }
       return false;
-    } finally {
+    } 
+    finally {
       setLoading(false);
     }
   };
@@ -48,14 +65,33 @@ export const useAuth = () => {
     try {
       await createUserWithEmailAndPassword(auth, email, password);
       return true;
-    } catch (error: unknown) {
-      if (error instanceof Error) {
+    } 
+    catch (error: unknown) {
+      
+      if (error instanceof FirebaseError) {
+        console.error("Firebase Error during set up:", error.code);
+        if (error.code === 'auth/email-already-in-use') {
+          setError('Email already in use');
+        }
+        else if (error.code === 'auth/invalid-email') {
+          setError('Invalid email');
+        }
+        else if (error.code === 'auth/weak-password') {
+          setError('Weak password');
+        }
+        else {
+          setError('An unknown error occurred');
+        }
+      }
+      else if (error instanceof Error) {
+        console.error("Error during login:", error);
         setError(error.message);
       } else {
         setError('An unknown error occurred');
       }
       return false;
-    } finally {
+    } 
+    finally {
       setLoading(false);
     }
   };
