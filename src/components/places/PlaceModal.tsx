a39d1d89ac6cc8fa105a5fa5d501da451c2e9675
DiegoRojo99 <diego99rojo@gmail.com>
2025-01-23 11:30:58 +0000

    Bookmark is now a button and date min, max and check

--- src/components/places/PlaceModal.tsx
+++ src/components/places/PlaceModal.tsx
@@ -1,19 +1,24 @@
 import React, { useState } from 'react';
 import { GoogleSearchResult } from '@/types/search';
+import { Trip } from '@/types/trip';
 
 interface PlaceModalProps {
+  trip: Trip;
   place: GoogleSearchResult;
   onClose: () => void;
   onSave: (place: GoogleSearchResult, notes: string, date: string, isBookmarkOnly: boolean) => void;
 }
 
-export const PlaceModal: React.FC<PlaceModalProps> = ({ place, onClose, onSave }) => {
+export const PlaceModal: React.FC<PlaceModalProps> = ({ trip, place, onClose, onSave }) => {
   const [notes, setNotes] = useState('');
   const [date, setDate] = useState('');
-  const [isBookmarkOnly, setIsBookmarkOnly] = useState(false);
 
-  const handleSave = () => {
-    onSave(place, notes, date, isBookmarkOnly);
+  const handleSave = (bookmarkOnly: boolean) => {
+    if(!bookmarkOnly && !date) {
+      alert('Please select a date.');
+      return;
+    }
+    onSave(place, notes, date, bookmarkOnly);
   };
 
   return (
@@ -33,28 +38,27 @@ export const PlaceModal: React.FC<PlaceModalProps> = ({ place, onClose, onSave }
           value={date}
           onChange={(e) => setDate(e.target.value)}
           className="w-full border border-gray-300 rounded-md p-2 mb-4"
+          min={trip.startDate}
+          max={trip.endDate}
         />
-        <div className="flex items-center mb-4">
-          <input
-            type="checkbox"
-            checked={isBookmarkOnly}
-            onChange={(e) => setIsBookmarkOnly(e.target.checked)}
-            className="mr-2"
-          />
-          <label className="text-gray-700">Bookmark only (don’t add to itinerary)</label>
-        </div>
         <div className="flex justify-end space-x-4">
+          <button
+            onClick={() => handleSave(true)}
+            className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-yellow-600"
+          >
+            Bookmark
+          </button>
+          <button
+            onClick={() => handleSave(false)}
+            className="px-4 py-2 bg-green-500 text-white rounded-md hover:bg-blue-600"
+          >
+            Save
+          </button>
           <button
             onClick={onClose}
             className="px-4 py-2 bg-gray-300 text-gray-700 rounded-md hover:bg-gray-400"
           >
             Cancel
-          </button>
-          <button
-            onClick={handleSave}
-            className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
-          >
-            Save
           </button>
         </div>
       </div>
