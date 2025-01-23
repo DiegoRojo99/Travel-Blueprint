211524f5fdf3b5e2b84ea30b7d567982e1fd93ea
DiegoRojo99 <diego99rojo@gmail.com>
2025-01-23 11:30:37 +0000

    Removed clock for now

--- src/components/places/PlaceItem.tsx
+++ src/components/places/PlaceItem.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { GoogleSearchResult } from '@/types/search';
 import Image from 'next/image';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faClock } from '@fortawesome/free-solid-svg-icons';
 
 interface PlaceItemProps {
   place: GoogleSearchResult;
@@ -15,7 +13,7 @@ const PlaceItem: React.FC<PlaceItemProps> = ({ place }) => {
         <div className="font-semibold text-gray-800">{place.name}</div>
         <div className="text-sm text-gray-600 mt-2">{"No description available."}</div>
         <div className="flex items-center mt-2 space-x-1 mt-auto">
-          <FontAwesomeIcon icon={faClock} size="lg" className="text-gray-500 mr-1" />
+          {/* <FontAwesomeIcon icon={faClock} size="lg" className="text-gray-500 mr-1" /> */}
         </div>
       </div>
       {place.photo_reference && (
