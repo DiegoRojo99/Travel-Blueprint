bbb09fd0f0627486376a274d08de51e5859996a6
DiegoRojo99 <diego99rojo@gmail.com>
2025-01-25 17:36:42 +0000

    Updates pc card sizes

--- src/components/places/PlaceBookmarks.tsx
+++ src/components/places/PlaceBookmarks.tsx
@@ -7,7 +7,7 @@ const PlaceBookmarks: React.FC<{ trip: Trip }> = ({ trip }) => {
   return (
     <div className="my-2 flex flex-wrap -mx-2">
       {trip.places.map((place) => (
-        <div key={place.place_id} className="w-full sm:w-1/2 px-2 mb-4">
+        <div key={place.place_id} className="w-full sm:w-1/2 lg:w-1/3 px-2 mb-4">
           <PlaceItem place={place} />
         </div>
       ))}
