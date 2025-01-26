c201c9203312a26aed943c6e1273346ba073f46e
DiegoRojo99 <diego99rojo@gmail.com>
2025-01-26 13:30:52 +0000

    Added some comments about DB functions

--- src/db/places.ts
+++ src/db/places.ts
@@ -3,6 +3,12 @@ import { db } from '@/utils/firebase';
 import { updateDoc, doc, getDoc } from 'firebase/firestore';
 import fetch from "node-fetch";
 
+/**
+ * Updates the places attribute of a trip object.
+ * @param {string} tripId - The id of the trip object to update.
+ * @param {GoogleSearchResult[]} places - The places array that is going to be updated.
+ * @returns {Promise<boolean>} A boolean indicating the result of the operation.
+ */
 export async function updateTripPlaces(tripId: string, places: GoogleSearchResult[]): Promise<boolean> {
   const tripRef = doc(db, 'Trips', tripId);
   
@@ -20,6 +26,11 @@ export async function updateTripPlaces(tripId: string, places: GoogleSearchResul
   }
 }
 
+/**
+ * Search places using Google Places API.
+ * @param {string} query - The query that will be used to search for places.
+ * @returns {Promise<GoogleSearchResult[]>} An array of places found matching the query.
+ */
 export async function searchGooglePlaces(query: string): Promise<GoogleSearchResult[]> {
   const apiKey = process.env.GOOGLE_PLACES_API_KEY;
   const apiUrl = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${encodeURIComponent(
