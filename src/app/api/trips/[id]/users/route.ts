e1178159d1838dd958fa11c87b0deaf0319f3229
DiegoRojo99 <diego99rojo@gmail.com>
2025-02-02 22:35:25 +0000

    Trip id and user id both in body

--- src/app/api/trips/[id]/users/route.ts
+++ src/app/api/trips/[id]/users/route.ts
@@ -1,19 +1,18 @@
 import { addUserToTrip } from "@/db/trips";
-import { context } from "@/types/routes";
 
-export async function POST(req: Request, context: context): Promise<Response> {
-  const { tripId } = await context.params;
-
+export async function POST(req: Request): Promise<Response> {
   try {
     const body = await req.json();
-    const { userId } = body;
+    const { userId, tripId } = body;
 
+    if (!tripId) {
+      return new Response(JSON.stringify({ error: "Trip ID is required" }), { status: 400 });
+    }
     if (!userId) {
       return new Response(JSON.stringify({ error: "User ID is required" }), { status: 400 });
     }
 
     await addUserToTrip(tripId, userId, "Member");
-
     return new Response(JSON.stringify({ message: `User ${userId} added to trip ${tripId}` }), { status: 200 });
   } catch (error) {
     console.error("Error adding user to trip:", error);
