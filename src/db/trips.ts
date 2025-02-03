e519ee4e4437bd2218e364d2e1aa7e574a78003d
DiegoRojo99 <diego99rojo@gmail.com>
2025-02-03 20:34:13 +0000

    Updated to admin DB

--- src/db/trips.ts
+++ src/db/trips.ts
@@ -1,9 +1,7 @@
 import { Trip, TripDocument } from '@/types/trip';
 import { UserDB } from '@/types/users';
-import { db } from '@/lib/firebase';
-import { collection, getDocs, addDoc, getDoc, updateDoc, doc, deleteDoc, arrayUnion } from 'firebase/firestore';
 import { adminDB } from '@/lib/firebaseAdmin';
-
+import { firestore } from 'firebase-admin';
 
 /**
  * Fetch trips from Firestore for a specific user
@@ -14,26 +12,11 @@ export const getUserTrips = async (userId: string): Promise<Trip[]> => {
   const tripsRef = adminDB.collection('Trips');
   const tripsQuery = tripsRef.where('userIds', 'array-contains', userId);
   const tripsSnapshot = await tripsQuery.get();
-
   const trips: Trip[] = tripsSnapshot.docs.map(doc => {
     const trip = doc.data() as TripDocument;
     return { id: doc.id, ...trip };
   });
 
-  return trips;
-};
-
-/**
- * Fetch all trips from Firestore
- * @returns {Promise<Trip[]>} A trip array indicating the result of the operation.
- */
-export const getTrips = async (): Promise<Trip[]> => {
-  const querySnapshot = await getDocs(collection(db, 'Trips'));
-  const trips: Trip[] = [];
-  querySnapshot.forEach((doc) => {
-    const trip = doc.data() as TripDocument;
-    trips.push({ id: doc.id, ...trip });
-  });
   return trips;
 };
 
@@ -44,7 +27,7 @@ export const getTrips = async (): Promise<Trip[]> => {
  */
 export const addTrip = async (tripData: Trip): Promise<string> => {
   try {
-    const docRef = await addDoc(collection(db, 'Trips'), tripData);
+    const docRef = await adminDB.collection('Trips').add(tripData);
     return docRef.id;
   } catch (e) {
     console.error('Error adding document: ', e);
@@ -61,15 +44,14 @@ export const getTripById = async (id: string): Promise<Trip> => {
   if(!id){
     throw new Error("Trip ID not provided");
   }
-  const tripsRef = adminDB.collection('Trips');
-  const tripsQuery = tripsRef.where('id', '==', id);
-  const tripSnapshot = await tripsQuery.get();
+  const tripsRef = adminDB.collection('Trips').doc(id)
+  const tripSnapshot = await tripsRef.get();
 
-  if (tripSnapshot.empty) {
+  if (!tripSnapshot.exists) {
     throw new Error("Trip not found");
   } 
   console.log("Trip Snap:", tripSnapshot);
-  const data = tripSnapshot.docs[0].data() as unknown as TripDocument;
+  const data = tripSnapshot.data() as unknown as TripDocument;
   return { id: id, ...data };
 };
 
@@ -80,8 +62,8 @@ export const getTripById = async (id: string): Promise<Trip> => {
  * @returns {Promise<Trip>} The trip object after being updated.
  */
 export const updateTrip = async (id: string, updatedData: Trip): Promise<Trip> => {
-  const tripRef = doc(db, 'Trips', id);
-  await updateDoc(tripRef, { ...updatedData });
+  const tripRef = adminDB.collection('Trips').doc(id);
+  await tripRef.update({ ...updatedData });
   return { ...updatedData };
 };
 
@@ -92,13 +74,12 @@ export const updateTrip = async (id: string, updatedData: Trip): Promise<Trip> =
  */
 export const deleteTrip = async (id: string): Promise<boolean> => {
   try {
-    const tripRef = doc(db, 'Trips', id);
-    await deleteDoc(tripRef);
+    const tripRef = adminDB.collection("Trips").doc(id);
+    await tripRef.delete();
     return true;
-  } 
-  catch (error) {
-    console.error('Error deleting trip:', error);
-    throw new Error('Failed to delete trip');
+  } catch (error) {
+    console.error("Error deleting trip:", error);
+    throw new Error("Failed to delete trip");
   }
 };
 
@@ -109,27 +90,45 @@ export const deleteTrip = async (id: string): Promise<boolean> => {
  * @param {string} role - The role the user to add(Owner, Member, Viewer).
  */
 export const addUserToTrip = async (tripId: string, userId: string, role: string): Promise<boolean> => {
-  const tripRef = doc(db, 'Trips', tripId);
-  const userRef = doc(db, "Users", userId);
-  const userDoc = await getDoc(userRef);
-
-  if(!userDoc.exists()) return false;
-
   try {
+    const tripsRef = adminDB.collection("Trips");
+    const tripsQuery = tripsRef.where("id", "==", tripId);
+    const tripsSnapshot = await tripsQuery.get();
+
+    if (tripsSnapshot.empty) {
+      console.error("Trip not found");
+      return false;
+    }
+
+    const tripDoc = tripsSnapshot.docs[0];
+    const tripRef = tripDoc.ref;
+
+    const userRef = adminDB.collection("Users");
+    const userQuery = userRef.where("id", "==", userId);
+    const userSnapshot = await userQuery.get();
+
+    if (userSnapshot.empty) {
+      console.error("User not found");
+      return false;
+    }
+
+    const userDoc = userSnapshot.docs[0];
     const userData = userDoc.data() as UserDB;
+
     const user = {
       uid: userData.id,
       displayName: userData.name,
       photoURL: userData.profilePicture,
       email: userData.email,
       role: role,
-    }
-    await updateDoc(tripRef, {
-      users: arrayUnion(user),
+    };
+
+    await tripRef.update({
+      users: firestore.FieldValue.arrayUnion(user),
     });
+
     return true;
-  } 
-  catch (error: unknown) {
+  } catch (error) {
     console.error("Error adding user to trip:", error);
     return false;
   }
