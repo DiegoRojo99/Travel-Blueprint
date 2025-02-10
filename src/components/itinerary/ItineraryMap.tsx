3ce281d1602ba310725196eb01297265ef367584
DiegoRojo99 <diego99rojo@gmail.com>
2025-02-10 21:20:18 +0000

    Removed click option

--- src/components/itinerary/ItineraryMap.tsx
+++ src/components/itinerary/ItineraryMap.tsx
@@ -8,17 +8,12 @@ const ItineraryMap = ({ trip }: { trip: Trip}) => {
     lng: trip?.destinations?.[0].location.lng || 0
   };
 
-  const handleClick = (event: google.maps.MapMouseEvent) => {
-    console.log('Clicked on map:', event);
-  };
-
   return (
     <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ""}>
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
         center={defaultCenter}
-        zoom={5}
-        onClick={handleClick}
+        zoom={12}
       >
         {trip.stops?.map((stop) => (
           <Marker key={stop.id} position={{ lat: stop.location.lat, lng: stop.location.lng }} />
